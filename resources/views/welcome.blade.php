c1a198f747ecf67da48f184db08cdf84ce471e24
Frixos <georgiospanagiotakopoulos@outlook.com>
2026-06-09 21:17:42 +0300

    various issues

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@props(['title' => 'Default Title'])
+<x-layout title="Panacodes">
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <title>Laravel + Tailwind Test</title>
-</head>
-<body class="bg-gray-100">
-
-    <div class="min-h-screen flex items-center justify-center">
-        <div class="bg-red-500 text-white p-10 rounded-lg shadow-lg">
-            Tailwind is working!
-        </div>
-    </div>
-
-</body>
-</html>
+</x-layout>
